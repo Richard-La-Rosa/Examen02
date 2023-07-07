5831d734a3c7abc204390fdf6038263f654a968a
Richard_La_Rosa <toastspeedruns@outlook.com.ar>
2023-07-06 23:47:24 -0500

    examen02

---
+++ app/src/main/java/com/example/examen02_l2/MapaActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.examen02_l2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapaActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mapa2)
+    }
+}
